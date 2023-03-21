067044eac076196c68d8472f7df1f17dc4eb4748
rudolph <mobile.sjs@gmail.com>
2023-03-21 13:21:30 +0900

    java 진수출력

--- c/test.c
+++ c/test.c
@@ -2,23 +2,54 @@
 #include <stdbool.h>
 
 int main(int args, char *argv[]) {
-    int n1 = 1, n2 = 2, n3 = 3;
-    int r1, r2, r3;
-    bool tf = -3;
+    // int n1 = 1, n2 = 2, n3 = 3;
+    // int r1, r2, r3;
+    // bool tf = -3;
 
-    r1 = (n2 <= 2) || (n3 > 3);
-    r2 = !n3;
-    r3 = (n1 > 1) && (n2 < 3);
+    // r1 = (n2 <= 2) || (n3 > 3);
+    // r2 = !n3;
+    // r3 = (n1 > 1) && (n2 < 3);
 
-    printf("%d\n", tf);
-    printf("%d > 1 = %d\n", n1, n1 > 1);
-    printf("%d, %d, %d\n", r1, r2, r3);
-    printf("%d\n", r3 - r2 + r1);
+    // printf("%d\n", tf);
+    // printf("%d > 1 = %d\n", n1, n1 > 1);
+    // printf("%d, %d, %d\n", r1, r2, r3);
+    // printf("%d\n", r3 - r2 + r1);
 
-    int a[] = {14, 22, 30, 38};
-    printf("%d\n", a[2]);
-    printf("%u\n", &a[2]);
-    printf("%u\n", a);
+    // int a[] = {14, 22, 30, 38};
+    // printf("%d\n", a[2]);
+    // printf("%u\n", &a[2]);
+    // printf("%u\n", a);
+  
 
+    //--------------------------
+    // int x = 5, a, b;
+    
+    // a = ++x*x--;
+    // b = x * 10;
+
+    // printf("%d\n", a);
+    // printf("%d\n", b);
+
+    int a = 10, b = 3, c = 1;
+    a *= (b-1);
+    b/=2+3;
+    c+=2;
+    printf("a=%d b=%d, c=%d\n", a,b,c);
+
+
+    float ff = 3.14;
+    printf("int형의 크기는=%d바이트\n", sizeof(int));
+    printf("float형의 크기는=%d바이트\n", sizeof(ff));
+    printf("double형의 크기는=%d바이트\n", sizeof(double));
+    printf("long형의 크기는=%d바이트\n", sizeof(long));
+
+
+    //--------------------------
+    int a1=10, b1=20, c1= 30;
+        
+    printf("%d\n", a1 = b1+=2*c1);          //c1 * 2 = 60 + b1(20) + 80 --> b1 = 80
+    //printf("%d\n", b1);
+    printf("%d\n", a1 = (b1 > c1) ? b1 : c1);       //b1 = 80으로 바뀐상태
+    
     return 0;
 }
--- java/Study/src/HelloWorld.java
+++ java/Study/src/HelloWorld.java
@@ -1,8 +1,39 @@
 
 public class HelloWorld {
 
+	public static int a = 100;
+
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		//System.out.println("Hello World");
+
+		int[] numbers = {2, -9, 0, 5, 12,-25,22,9,8,12};
+
+		int sum = 0;
+		for (int number : numbers) {
+			sum += number;
+		}
+
+		double average;
+		int alen = numbers.length;
+		average = (double) sum / alen;
+
+		System.out.println("합 = " + sum);
+		System.out.println("평균 = " + average);
+
+
+		System.out.println("--------------------------------------");
+		//진수출력
+		int data = 10;
+
+		System.out.println(Integer.toBinaryString(data));		//2진수
+		System.out.println(Integer.toOctalString(data));		//8진수
+		System.out.println(Integer.toHexString(data));		//16진수
+
+		System.out.println(Integer.parseInt("1010", 2));	//2->10진수
+		System.out.println(Integer.parseInt("12", 8));	//8->10진수
+		System.out.println(Integer.parseInt("a", 16));	//16->10진수
+
+		
 	}
 
 }
