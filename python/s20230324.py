1a5ec3e596cbba5b7156e605aaa92d97f1153f6e
rudolph <mobile.sjs@gmail.com>
2023-04-04 10:52:18 +0900

    구구단

---
+++ python/s20230324.py
@@ -0,0 +1,28 @@
+print(3<6)
+
+print(3 == 6)
+
+
+temp = 20;
+fruit = "apple";
+
+if temp >= 20 and fruit == "apple":
+    print("true")
+else:
+    print("false")
+
+
+
+# rad = int(input("반지름 : "))
+# hei = int(input("높이 : "))
+
+# if rad > 0 and hei > 0:
+#     vol = 1/3 * 3.14 * rad ** 2 * hei
+#     suf = 3.14 * rad ** 2 + 3.14 * rad * hei
+#     print("원뿔 부피 : ", vol)
+#     print("원뿔 겉넓이 : ", suf)
+
+
+vv = input("숫자 3개 입력 : " )
+print(vv[0], vv[1], vv[2], sep='')
+
