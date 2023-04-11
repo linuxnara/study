dddf348ffb7e506aaad6e1d7a78289fff9af251a
rudolph <mobile.sjs@gmail.com>
2023-04-11 11:14:55 +0900

    h2

--- c/work/h1_2.c
+++ c/work/h1_2.c
@@ -1,11 +1,11 @@
 /* 이 프로그램은 두 수를 키보드로 입력받아 곱의 결과를 출력하는 프로그램이다 */
 
 /* 도입부 */
-#include <stdio.h> 					//printf()와 scanf()를 사용하기 위한 헤더파일 포함 
-#pragma warning(disable:4996))		//권장하지 않는 함수 사용에 대한 경고 메시지 무시 
+#include <stdio.h> 					//printf()와 scanf()를 사용하기 위한 헤더 파일 포함 
+#pragma warning(disable:4996))		//권장하지 않는 함수 사용에 대한 경고 메시지 무시
 
 int a,b,c;						//정수를 저장하는 변수 선언
-int product(int x, int y);		//사용자 함수 원형 
+int product(int x, int y);		//사용자 함수 원형
 
 /* main 함수 */
 void main() {
@@ -13,7 +13,7 @@ void main() {
 	printf("Enter a number between 1 and 100 : ");
 	scanf("%d", &a);
 	
-	/* 두 번째 수를 입력 */
+	/* 두 번째 수를 입력*/
 	printf("Enter another number between 1 and 100 : ");
 	scanf("%d", &b);
 	
@@ -22,6 +22,7 @@ void main() {
 	printf("%d * %d = %d\n", a, b, c);
 } 
 
+/* 두 수의 곱을 계산하여 돌려주는 사용자 함수 */
 int product(int x, int y) {
 	return (x * y);
 }
--- c/work/h2_1.c
+++ c/work/h2_1.c
@@ -2,4 +2,23 @@
 
 void main() {
 	printf("char 자료형 크기 : %d byte\n", sizeof(char));
+	printf("signed char 자료형 크기 : %d byte\n", sizeof(signed char));
+	printf("short 자료형 크기 : %d byte\n", sizeof(short));
+	printf("short int 자료형 크기 : %d byte\n", sizeof(short int));
+	printf("signed short 자료형 크기 : %d byte\n", sizeof(signed short));
+	printf("signed short int 자료형 크기 : %d byte\n", sizeof(signed short int));
+	printf("unsigned short 자료형 크기 : %d byte\n", sizeof(unsigned short));
+	printf("unsigned short int 자료형 크기 : %d byte\n", sizeof(unsigned short int));
+	printf("int 자료형 크기 : %d byte\n", sizeof(int));
+	printf("signed int 자료형 크기 : %d byte\n", sizeof(signed int));
+	printf("unsigned 자료형 크기 : %d byte\n", sizeof(unsigned));
+	printf("unsigned int 자료형 크기 : %d byte\n", sizeof(unsigned int));
+	printf("long 자료형 크기 : %d byte\n", sizeof(long));
+	printf("long int 자료형 크기 : %d byte\n", sizeof(long int));
+	printf("signed long 자료형 크기 : %d byte\n", sizeof(signed long));
+	printf("signed long int 자료형 크기 : %d byte\n", sizeof(signed long int));
+	printf("int 자료형 크기 : %d byte\n", sizeof(int));
+	printf("int 자료형 크기 : %d byte\n", sizeof(int));
+	printf("int 자료형 크기 : %d byte\n", sizeof(int));
+	printf("int 자료형 크기 : %d byte\n", sizeof(int));
 }
